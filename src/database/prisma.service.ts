a0e3468e40d08381fd989338a27e6a3f4d289401
David Cano <davidsantiago03.com@gmail.com>
2025-10-15 12:49:34 -0500

    chore: configuracion inicial de NestJS

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+    console.log('Connected to the database');
+  }
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+    console.log('Disconnected from the database');
+  }
+}
